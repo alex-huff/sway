3891d8509a706563961ef6894e4013e8d6d1e824
alex-huff <alexhuff@bakahuff.com>
2025-09-27 17:36:27 -0500

    share titlebar borders vertically aswell

--- include/sway/tree/container.h
+++ include/sway/tree/container.h
@@ -78,6 +78,7 @@ struct sway_container {
 
 		struct wlr_scene_tree *border;
 		struct wlr_scene_tree *background;
+		struct wlr_scene_tree *filler;
 
 		struct sway_text_node *title_text;
 		struct sway_text_node *marks_text;
